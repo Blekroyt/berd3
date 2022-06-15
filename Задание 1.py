a8894288e348cbc42311ddb1b7a70033c5220f81
Blekroyt <99321528+Blekroyt@users.noreply.github.com>
2022-06-15 18:58:25 +0300

    Update Задание 1.py

--- Задание 1.py
+++ Задание 1.py
@@ -3,8 +3,15 @@
 import math
 import sys
 
-def new_func():
-    ex = int(input("Введите число экзаменов: "))
-    print(f"Мы успешно сдали { ex } экзаменов!")
-
-new_func()
+if __name__ == '__main__':
+    n = int(input("n = "))
+    if n > 20:
+        print("Ошибка", file=sys.stderr)
+        exit(1)
+    if n == 1:
+        b = " экзаменов"
+    elif n <= 20:
+        b = " экзаменов"
+    else:
+        b = " экзаменов"
+    print("Мы успешно сдали ", n, b)
